9ccd7810119902aeeeb9eafeaad29158ff8e7356
jhs512 <jangka512@gmail.com>
2020-10-11 17:28:15 +0900

    게시물 리스트에서 page 파라미터에 의해 페이지 구분

--- src/main/java/com/sbs/example/lolHi/dao/ArticleDao.java
+++ src/main/java/com/sbs/example/lolHi/dao/ArticleDao.java
@@ -10,7 +10,7 @@ import com.sbs.example.lolHi.dto.Article;
 
 @Mapper
 public interface ArticleDao {
-	List<Article> getArticles();
+	List<Article> getArticles(Map<String, Object> param);
 
 	Article getArticleById(@Param("id") int id);
 
