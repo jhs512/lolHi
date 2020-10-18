43350566114a9a2cc9c1039e6b05385770510be9
jhs512 <jangka512@gmail.com>
2020-10-18 17:30:33 +0900

    로그아웃

--- src/main/java/com/sbs/example/lolHi/controller/usr/MemberController.java
+++ src/main/java/com/sbs/example/lolHi/controller/usr/MemberController.java
@@ -48,6 +48,14 @@ public class MemberController {
 				member.getName());
 	}
 
+	@RequestMapping("/usr/member/doLogout")
+	@ResponseBody
+	public String doLogout(HttpSession session) {
+		session.removeAttribute("loginedMemberId");
+
+		return String.format("<script> location.replace('/usr/article/list'); </script>");
+	}
+
 	@RequestMapping("/usr/member/join")
 	public String showJoin() {
 		return "usr/member/join";
