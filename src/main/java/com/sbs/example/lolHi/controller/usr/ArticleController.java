3da415cf85876fd5235394cdb565af0622205338
jhs512 <jangka512@gmail.com>
2020-10-24 16:43:46 +0900

    게시물 수정시 권한체크

--- src/main/java/com/sbs/example/lolHi/controller/usr/ArticleController.java
+++ src/main/java/com/sbs/example/lolHi/controller/usr/ArticleController.java
@@ -93,8 +93,14 @@ public class ArticleController {
 			model.addAttribute("replaceUri", "/usr/member/login");
 			return "common/redirect";
 		}
-
+		
 		Article article = articleService.getArticleById(id);
+		
+		if ( article.getMemberId() != loginedMemberId ) {
+			model.addAttribute("msg", "권한이 없습니다.");
+			model.addAttribute("historyBack", true);
+			return "common/redirect";
+		}
 
 		model.addAttribute("article", article);
 
@@ -113,6 +119,14 @@ public class ArticleController {
 		if (loginedMemberId == 0) {
 			model.addAttribute("msg", "로그인 후 이용해주세요.");
 			model.addAttribute("replaceUri", "/usr/member/login");
+			return "common/redirect";
+		}
+		
+		Article article = articleService.getArticleById(id);
+		
+		if ( article.getMemberId() != loginedMemberId ) {
+			model.addAttribute("msg", "권한이 없습니다.");
+			model.addAttribute("historyBack", true);
 			return "common/redirect";
 		}
 
