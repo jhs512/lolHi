21888985c7a32349dc0701ef2aeace002a89c4d8
jhs512 <jangka512@gmail.com>
2020-11-14 17:47:06 +0900

    회원가입시 가입축하메일 발송

--- src/main/java/com/sbs/example/lolHi/service/MemberService.java
+++ src/main/java/com/sbs/example/lolHi/service/MemberService.java
@@ -3,6 +3,7 @@ package com.sbs.example.lolHi.service;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.sbs.example.lolHi.dao.MemberDao;
@@ -11,15 +12,36 @@ import com.sbs.example.lolHi.util.Util;
 
 @Service
 public class MemberService {
+	@Value("${custom.siteName}")
+	private String siteName;
+	
+	@Value("${custom.siteMainUri}")
+	private String siteMainUri;
+	
 	@Autowired
 	private MemberDao memberDao;
+	
+	@Autowired
+	private MailService mailService;
 
 	public int join(Map<String, Object> param) {
 		memberDao.join(param);
 
 		int id = Util.getAsInt(param.get("id"));
 
+		sendJoinCompleteMail((String) param.get("email"));
+
 		return id;
+	}
+
+	private void sendJoinCompleteMail(String email) {
+		String mailTitle = String.format("[%s] 가입이 완료되었습니다.", siteName);
+
+		StringBuilder mailBodySb = new StringBuilder();
+		mailBodySb.append("<h1>가입이 완료되었습니다.</h1>");
+		mailBodySb.append(String.format("<p><a href=\"%s\" target=\"_blank\">%s</a>로 이동</p>", siteMainUri, siteName));
+
+		mailService.send(email, mailTitle, mailBodySb.toString());
 	}
 
 	public boolean isJoinAvailableLoginId(String loginId) {
@@ -51,7 +73,7 @@ public class MemberService {
 		if (email == null || email.length() == 0) {
 			return false;
 		}
-		
+
 		Member member = memberDao.getMemberByNameAndEmail(name, email);
 
 		return member == null;
